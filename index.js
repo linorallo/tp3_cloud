376bc4e083ab5c8f5f65144f4fb69ee0ddc43e1c
Lino Rallo <linorallo@gmail.com>
2020-10-03 14:18:20 -0300

    Fix

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var AWS = require('aws-sdk');
+
 var handler = async (event) => {
     var dynamodb = new AWS.DynamoDB({
         apiVersion: '2012-08-10',
@@ -14,6 +15,34 @@ var handler = async (event) => {
         service: dynamodb
     });
     // codigo de la funcion
-    console.log(dynamodb.listTables());
-}
+    //console.log(dynamodb.listTables());
+
+    switch (event.httpMethod) {
+        case 'GET':
+            var params = {
+                TableName: 'Envio',
+                FilterExpression: 'attribute_exists(pendiente)'
+            };
+
+            return await docClient
+                .scan(params)
+                .promise()
+                .then((data) => {
+                    return {
+                        statusCode: 200,
+                        body: JSON.stringify(data.Items),
+                    };
+                })
+                .catch((err) => {
+                    console.log(err);
+                    return {
+                        statusCode: 500,
+                        body: err.message,
+                    };
+
+                });
+
+
+    }
+};
 exports.handler = handler;
