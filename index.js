bfac113a3c29d05e2f46c121bb79f7969ff94726
Lino Rallo <linorallo@gmail.com>
2020-10-03 22:03:14 -0300

    funcionando

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var AWS = require('aws-sdk');
+var uniqid = require('uniqid');
 
 var handler = async (event) => {
     var dynamodb = new AWS.DynamoDB({
@@ -17,11 +18,13 @@ var handler = async (event) => {
     // codigo de la funcion
     //console.log(dynamodb.listTables());
 
+    var table = 'Envio';
+
     switch (event.httpMethod) {
-        case 'GET':
-            var params = {
-                TableName: 'Envio',
-                FilterExpression: 'attribute_exists(pendiente)'
+        case "GET":
+            const params = {
+                TableName: table,
+                FilterExpression: "attribute_exists(pendiente)"
             };
 
             return await docClient
@@ -39,10 +42,80 @@ var handler = async (event) => {
                         statusCode: 500,
                         body: err.message,
                     };
-
                 });
 
+        case "PUT":
+            if (event.pathParameters.idEnvio) {
+                const idEnvio = event.pathParameters.idEnvio;
+                const params = {
+                    TableName: table,
+                    Key: {
+                        id: idEnvio
+                    },
+                    UpdateExpression: "REMOVE pendiente",
+                    ReturnValues: "UPDATED_NEW"
+                };
+                return await docClient
+                    .update(params)
+                    .promise()
+                    .then((data) => {
+                        return {
+                            statusCode: 200,
+                            body: JSON.stringify(params),
+                        };
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                        return {
+                            statusCode: 500,
+                            body: err.message,
+                        };
+                    });
+            } else {
+                return {
+                    statusCode: 400,
+                    body: "Bad Request",
+                };
+            }
 
+        case 'POST':
+            if (event.body){
+                const params = {
+                    TableName: table,
+                    Item: {
+                        id: uniqid(),
+                        pendiente: new Date().toISOString(),
+                        fechaAlta: new Date().toISOString(),
+                        destino: JSON.parse(event.body).destino,
+                        email: JSON.parse(event.body).email,
+                    },
+                };
+    
+                return await docClient
+                    .put(params)
+                    .promise()
+                    .then((data) => {
+                        return {
+                            statusCode: 200,
+                            body: JSON.stringify(params)
+                        };
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                        return {
+                            statusCode: 500,
+                            body: err.message,
+                        };
+                    });
+            }
+            else{
+                return{
+                    statusCode:400,
+                    body: 'Bad Request',
+                };
+            }
     }
+
+
 };
 exports.handler = handler;
